d4304ae23996188c7cdfe16f7501b0c3b3eabf06
Rohan Manuel . A <130838957+RohanManuel@users.noreply.github.com>
2024-08-10 15:42:07 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://dandwportal.onrender.com";
-export const JOB_API_END_POINT="https://dandwportal.onrender.com";
-export const APPLICATION_API_END_POINT="https://dandwportal.onrender.com";
-export const COMPANY_API_END_POINT="https://dandwportal.onrender.com";
+export const USER_API_END_POINT="https://jobportal-backend-70nx.onrender.com";
+export const JOB_API_END_POINT="https://jobportal-backend-70nx.onrender.com";
+export const APPLICATION_API_END_POINT="https://jobportal-backend-70nx.onrender.com";
+export const COMPANY_API_END_POINT="https://jobportal-backend-70nx.onrender.com";
