709037cf11558db6b78e0fd6652401fe08c8e019
Rohan Manuel . A <130838957+RohanManuel@users.noreply.github.com>
2024-08-10 18:33:59 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://job-portal-backend-beryl.vercel.app/api/v1/user";
-export const JOB_API_END_POINT="https://job-portal-backend-beryl.vercel.app/api/v1/job";
-export const APPLICATION_API_END_POINT="https://job-portal-backend-beryl.vercel.app/api/v1/application";
-export const COMPANY_API_END_POINT="https://job-portal-backend-beryl.vercel.app/api/v1/company";
+export const USER_API_END_POINT="https://job-portal-backend-80hc3bf9c-rohanmanuels-projects.vercel.app/api/v1/user";
+export const JOB_API_END_POINT="https://job-portal-backend-80hc3bf9c-rohanmanuels-projects.vercel.app/api/v1/job";
+export const APPLICATION_API_END_POINT="https://job-portal-backend-80hc3bf9c-rohanmanuels-projects.vercel.app/api/v1/application";
+export const COMPANY_API_END_POINT="https://job-portal-backend-80hc3bf9c-rohanmanuels-projects.vercel.app/api/v1/company";
