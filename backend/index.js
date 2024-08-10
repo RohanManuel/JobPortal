9c2b01940212f53a07d21ba9f2a82891ab22c90a
Rohan Manuel . A <130838957+RohanManuel@users.noreply.github.com>
2024-08-10 18:53:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,31 +12,29 @@ dotenv.config({});
 
 const app = express();
 
-// Middleware
+// middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-
-// CORS configuration
 const corsOptions = {
-    origin: 'https://job-portal-frontend-mrjx0wt4v-rohanmanuels-projects.vercel.app', // No trailing slash
-    credentials: true, // Allow credentials (cookies, etc.)
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-};
+    origin:'http://localhost:5173',
+    credentials:true
+}
 
 app.use(cors(corsOptions));
 
-// Routes
+const PORT = process.env.PORT || 3000;
+
+
+// api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-const PORT = process.env.PORT || 3000;
 
-// Start the server
-app.listen(PORT, () => {
+
+app.listen(PORT,()=>{
     connectDB();
     console.log(`Server running at port ${PORT}`);
-});
+})
