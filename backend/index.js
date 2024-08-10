9964f5777781a7e693854ea007eb76218972b4bf
Rohan Manuel . A <130838957+RohanManuel@users.noreply.github.com>
2024-08-10 18:30:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-portal-frontend-navy.vercel.app/',
+    origin:'https://job-portal-frontend-navy.vercel.app',
     credentials:true
 }
 
